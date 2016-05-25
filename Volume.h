bfbe735699bfdbb103cdb99a7f2180d799d91b8f
connornishijima <connornishijima@gmail.com>
2016-05-25 09:53:16 -0600

    Create Volume.h

---
+++ Volume.h
@@ -0,0 +1,27 @@
+/*
+  Volume.h - Library for tone() with 8-bit volume control.
+  Created by Connor Nishijima, May 25th 2016.
+  Released into the public domain.
+*/
+#ifndef volume_h
+#define volume_h
+
+#include "Arduino.h"
+
+class Volume
+{
+  public:
+    Volume(int pin);
+	void begin();
+	void end();
+    void tone(int frequency, byte volume);
+    void noTone();
+	void delay(unsigned long d);
+	void delayMicroseconds(unsigned long du);
+	void setMasterVolume(float mv);
+  private:
+    int _pin;
+
+};
+
+#endif
