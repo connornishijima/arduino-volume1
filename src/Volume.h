5277487dc7ce11277be36d2cb76597b04a79280b
Connor Nishijima <connornishijima@gmail.com>
2016-06-14 01:25:12 -0600

    Update Volume.h

--- src/Volume.h
+++ src/Volume.h
@@ -1,6 +1,7 @@
 /*
   Volume.h - Library for tone() with 8-bit volume control.
   Created by Connor Nishijima, May 25th 2016.
+  Updated June 14, 2016.
   Released under the GPLv3 license.
 */
 #ifndef volume_h
@@ -17,7 +18,10 @@ class Volume
     void alternatePin(bool enabled);
     void throwError(byte errorNum);
     void tone(int frequency, byte volume);
+    void fadeOut(int duration);
     void noTone();
+    unsigned long millis();
+    unsigned long micros();
     void delay(unsigned long d);
     void delayMicroseconds(unsigned long du);
     void setMasterVolume(float mv);
